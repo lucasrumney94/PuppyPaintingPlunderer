2db61a67515c3a0d36beb5ce208dff9d25693636
Xueer Zhu <zhuxueerwhitney@gmail.com>
2016-12-21 21:27:44 -0500

    Add door highlight feature in doorEnter.cs

---
+++ PuppyPaintingPlunderer/Assets/Script/doorEnter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Once a player's head collide with the door collider, give feedback -> highlight door 
+public class doorEnter : MonoBehaviour {
+
+	[SerializeField]
+	private Transform playerTransform;
+
+	[SerializeField]
+	private GameObject doorHighlight;
+
+	[SerializeField]
+	private Vector3 nextLevelDoor;
+
+
+	void start() {
+		playerTransform = GameObject.FindGameObjectWithTag ("MainCamera").transform;
+		doorHighlight = GameObject.FindGameObjectWithTag ("Highlight");
+	}
+
+	void update() {
+ 		
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag.Equals ("MainCamera")) {
+			doorHighlight.SetActive(true);
+		}
+	}
+}
